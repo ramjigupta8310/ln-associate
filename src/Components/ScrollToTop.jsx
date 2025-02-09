26bf219e26274889e280c875afcbdcb16f96e2ed
Ramji Gupta <ramjigupta8310@gmail.com>
2025-02-10 02:13:32 +0530

    updating ScrollToTop.jsx

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -6,14 +6,15 @@ const ScrollToTop = () => {
 
     useEffect(() => {
         setTimeout(() => {
-          window.scrollTo({ top: 0, behavior: "instant" }); // Instant reset first
-        //   setTimeout(() => {
-        //     window.scrollTo({ top: 0, behavior: "smooth" }); // Then smooth scroll
-        //   }, 50); // Small delay for smooth effect
-        },  50); // Delay ensures DOM is loaded before scroll reset
-      }, [pathname]);
+            window.history.scrollRestoration = "manual"; // Force disable browser scroll restore
+            window.scrollTo({ top: 0, behavior: "instant" }); // Instant reset first
+            //   setTimeout(() => {
+            //     window.scrollTo({ top: 0, behavior: "smooth" }); // Then smooth scroll
+            //   }, 50); // Small delay for smooth effect
+        }, 50); // Delay ensures DOM is loaded before scroll reset
+    }, [pathname]);
 
-  
+
 
     return null;
 }
